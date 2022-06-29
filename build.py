e6a912ff2ebaa81c52a36382fce5adca0035a7d6
TBIL 2021 Fellow <steven.clontz+tbil2021@gmail.com>
2022-06-29 18:54:36 +0000

    add full build script

---
+++ build.py
@@ -0,0 +1,10 @@
+import logging, subprocess
+
+print('generating assets')
+subprocess.run(["pretext", "generate", "-t", "web", '--all-formats'])
+print('building web')
+subprocess.run(["pretext", "build", 'web'])
+print('building instructor')
+subprocess.run(["pretext", "build", 'web-instructor'])
+print('building print')
+subprocess.run(["pretext", "build", 'print'])
