9403e6c3e3ea33c1b0068a9ab0b7b510fe428214
TBIL 2021 Fellow <steven.clontz+tbil2021@gmail.com>
2022-07-02 02:51:58 +0000

    updates to the build script

--- build.py
+++ build.py
@@ -1,20 +1,25 @@
-import logging, subprocess, shutil
+import logging, subprocess, shutil, pathlib, os
 from checkit.bank import Bank
 
 print('generating assets')
 subprocess.run(["pretext", "generate", "-t", "web", '--all-formats'])
 subprocess.run(["pretext", "generate", "-t", "print", '--all-formats'])
 print('building web')
-subprocess.run(["pretext", "build", 'web'])
+subprocess.run(["pretext", "build", 'web', '--clean'])
 print('building instructor')
-subprocess.run(["pretext", "build", 'web-instructor'])
+subprocess.run(["pretext", "build", 'web-instructor', '--clean'])
 print('building print')
-subprocess.run(["pretext", "build", 'print'])
+subprocess.run(["pretext", "build", 'print', '--clean'])
 
-# FIXME
-# print("regenerating exercises (no diagrams)")
-# b = Bank("exercises")
-# b.write_json(regenerate=True)
-# print("building checkit viewer")
-# b.build_viewer()
-# shutil.copytree("exercises/docs","output/deploy/2022/exercises",dirs_exist_ok=True)
+cwd = os.getcwd()
+os.chdir(os.path.join(cwd,'exercises'))
+try:
+    print("regenerating exercises (no diagrams)")
+    b = Bank()
+    b.write_json(regenerate=True)
+    print("building checkit viewer")
+    b.build_viewer()
+    shutil.rmtree("../output/deploy/2022/exercises")
+    shutil.copytree("docs","../output/deploy/2022/exercises",dirs_exist_ok=True)
+finally:
+    os.chdir(cwd)
