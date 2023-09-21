5c4646f5fd0d2ec604e203408451efdb47d22f46
Steven Clontz <steven.clontz@gmail.com>
2023-09-21 19:40:33 +0000

    regenerate seeds on checkit preview, including images

--- scripts/bank/preview_outcome.py
+++ scripts/bank/preview_outcome.py
@@ -4,7 +4,7 @@ import subprocess
 os.chdir("exercises/sandbox")
 b = Bank()
 print('generate exercises')
-b.generate_exercises()
+b.generate_exercises(regenerate=True, images=True)
 b.write_json()
 b.build_viewer()
 subprocess.run(['python', '-m', 'http.server', '-d', 'docs'])
